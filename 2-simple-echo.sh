dea258801bdd30e3d3d9430477c964cf59076af1
Abhijeet <abhijeet3016@gmail.com>
2024-07-06 06:17:37 -0400

    add more exercises

---
+++ 2-simple-echo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Script to print "Hello, World!" to the terminal
+
+echo "Hello, World!"
+# Echo command: Prints the string "Hello, World!" to the terminal
---
+++ 3-cat-command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Script to display the contents of a text file named "sample.txt" using the cat command
+
+# Displaying the contents of the file named " sample.txt"
+cat 3-sample.txt
+# Cat command: Prints the contents of the specified file (in this case, " sample.txt") to the terminal
