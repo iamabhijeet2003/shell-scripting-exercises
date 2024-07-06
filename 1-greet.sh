a5a4f6c5198c4f5f7225617d8f8b85bdded2d233
Abhijeet <abhijeet3016@gmail.com>
2024-07-06 06:04:47 -0400

    add exercise 1

---
+++ 1-greet.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Script to greet the user with their name
+
+# Prompt the user to enter their name
+echo "Input your name:"
+
+# Read user input and store it in a variable named 'name'
+read name
+
+# Greet the user with their name
+echo "Hello, $name!"
