36c3c3258e48b45bf3878dfa523e4cd0ab49b4a7
Abhijeet <abhijeet3016@gmail.com>
2024-07-28 14:06:21 -0400

    add new exercise

---
+++ variables/6-variable-reassignment.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Variable declaration and initial assignment
+x=100
+
+# Reassigning the variable to a different value
+x=200
+
+# Printing the updated value of the variable
+echo "The updated value of x is: $x"
