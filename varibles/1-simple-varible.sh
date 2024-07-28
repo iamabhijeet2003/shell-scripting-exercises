cc2b087652e2d654b2cbd7dbe3e9aa82af5f3e27
Abhijeet <abhijeet3016@gmail.com>
2024-07-28 14:01:31 -0400

    add new exercises

---
+++ varibles/1-simple-varible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Variable declaration and assignment
+name="Marcela"
+
+# Printing the value of the variable
+echo "The value of the variable 'name' is: $name"
---
+++ varibles/2-variable-interpolation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
+
+# Variable declaration and assignment
+firstName="Abhijeet"
+lastName="Singh"
+
+# Printing a message greeting yourself using variable interpolation
+echo "Hello, $firstName $lastName! Nice to meet you."
