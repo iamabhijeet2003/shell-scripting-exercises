d96d2b4b03914f7c321adb4506632160befea660
Abhijeet <abhijeet3016@gmail.com>
2024-07-28 17:45:06 -0400

    modify directory

--- varibles/3-numeric-varible.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
-
-# Variable declaration and assignment
-age=20
-
-# Printing a message including your age
-echo "I am $age years old."
--- varibles/5-varible-concatenation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Shebang line: Indicates the path to the shell interpreter (in this case, bash)
-
-# Variable declaration and assignment
-var1="Hello"
-var2="World"
-
-# Concatenating the variables and printing the result
-echo "$var1 $var2"
